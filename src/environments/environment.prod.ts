534c58382c4bd42c633be1f89fddc43b4e546530
Martín Salvador Sosa <104036719+Tinincho@users.noreply.github.com>
2022-07-02 20:45:25 -0300

    Firebase publish

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAhXeSRNeElQ82P0LBiYZ08ZIeur3aWHj0",
+    authDomain: "share-costs-10320.firebaseapp.com",
+    projectId: "share-costs-10320",
+    storageBucket: "share-costs-10320.appspot.com",
+    messagingSenderId: "154240766413",
+    appId: "1:154240766413:web:7e5d46c46161816e41b484"
+  }
 };
