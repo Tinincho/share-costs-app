77f0c5f6c083f6de7ce7c99150d5f3cfd4f5be9f
Martín Salvador Sosa <104036719+Tinincho@users.noreply.github.com>
2022-07-20 15:09:03 -0300

    Edit calculation

--- src/app/components/cost-calculator/cost-calculator.component.ts
+++ src/app/components/cost-calculator/cost-calculator.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormsModule, ReactiveFormsModule , FormGroup } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule, FormGroup } from '@angular/forms';
 import { Contact } from 'src/app/models/contact.model';
 import { Product } from 'src/app/models/product.model';
 import { ContactService } from 'src/app/services/contact.service';
@@ -25,61 +25,80 @@ export class CostCalculatorComponent implements OnInit {
     if (!this.contactProducts.includes(getProduct)) {
 
       this.contactProducts.push(getProduct);
+      getProduct.cantityOfOwners++;
     } else {
 
       this.contactProducts.splice(this.contactProducts.indexOf(getProduct), 1);
+      getProduct.cantityOfOwners--;
     }
   }
 
   createContact(getContactString: string): void {
-    console.log(this.contactsList.find(contact => contact.name ===  getContactString.split(".")[0]));
-    
-   
-  if(!this.contactsList.find(contact => contact.name ===  getContactString.split(".")[0])){
+    //console.log(this.contactsList.find(contact => contact.name ===  getContactString.split(".")[0]));
 
-    
-    this.productService.productsArray.forEach(element => {
+    if (!this.contactsList.find(contact => contact.name === getContactString.split(".")[0])) {
 
-      if (this.contactProducts.includes(element)) {
+      /*
+      this.productService.productsArray.forEach(element => {
 
-        element.cantityOfOwners++;
-      }
-    });
+        if (this.contactProducts.includes(element)) {
 
-    var newContact = new Contact(getContactString.split(".")[0], getContactString.split(".")[1]);
+          element.cantityOfOwners++;
+        }
+      });
+      */
 
-    newContact.setProducts(this.contactProducts.slice()) ;
+      var newContact = new Contact(getContactString.split(".")[0], getContactString.split(".")[1]);
 
-    this.contactsList.push(newContact);
+      newContact.setProducts(this.contactProducts.slice());
 
-    console.log(this.productService.productsArray);
-    console.log(this.contactProducts);
-    console.log(this.contactsList);
+      this.contactsList.push(newContact);
+
+      /*
+      console.log(this.productService.productsArray);
+      console.log(this.contactProducts);
+      console.log(this.contactsList);
+      */
+    }
   }
+
+  seeProducts(getContactString: string): void {
+    
+    var getContact = this.contactsList.find(contact => contact.name === getContactString.split(".")[0]);
+
+    if (getContact == undefined) {
+
+      getContact = new Contact(getContactString.split(".")[0], getContactString.split(".")[1]);
+      console.log("No debería pasar");
+    }
+
+    this.contactProducts = [];
+    this.contactProducts = getContact.productsOwned.slice();
+    getContact.setProducts(this.contactProducts);
   }
 
   calculateCosts(): void {
     this.contactsList.forEach(contactIterator => {
+
       contactIterator.setTotalToPay(0);
-      console.log(contactIterator.productsOwned);
+
       contactIterator.productsOwned.forEach(element => {
 
         contactIterator.totalToPay += element.price / element.cantityOfOwners;
+        console.log(element.cantityOfOwners);
       });
     });
 
+    /*
     console.log(this.productService.productsArray);
     console.log(this.contactProducts);
     console.log(this.contactsList);
+    */
   }
   sendMessage(contact: Contact): void {
 
     window.open(`https://web.whatsapp.com/send?phone=${contact.phoneNumber}&text=${contact.name} tiene que pagar $${contact.totalToPay}`, '_blank');
-    // window.open(`https://wa.me/${contact.phoneNumber}?text=${contact.name}tienequepagar$${contact.totalToPay}`, '_blank');
     const keyEvent = new KeyboardEvent("keydown", { key: "Enter" });
-
-    //Trigger Eneter + Shift key Press
-    // const keyEvent = new KeyboardEvent("keydown", { key: "Enter", shiftKey: true });
     document.body.dispatchEvent(keyEvent);
   }
 
